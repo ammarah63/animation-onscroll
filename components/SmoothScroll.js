3bc0b13c792d6a71f9b8f8cdc6fa539d45ef82e9
ammarah63 <ammarahkhaleeq@gmail.com>
2024-08-19 14:00:02 +0500

    first commit

---
+++ components/SmoothScroll.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      lerp: 0.1,
+      duration: 1.2,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+};
+
+export default SmoothScroll;
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,165 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger, ScrollToPlugin } from "gsap";
+import SmoothScroll from "@/components/SmoothScroll";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import Logo from "@/public/Group 3005.svg";
+import SvgTransition from "@/components/SvgTransition";
 
-const inter = Inter({ subsets: ["latin"] });
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-export default function Home() {
+const Home = () => {
+  const containerRef = useRef(null);
+  const containerRef1 = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const maxScrollY = document.body.scrollHeight - window.innerHeight;
+      const scrollPercentage = scrollY / maxScrollY;
+
+      const scrollWidth = containerRef.current.scrollWidth;
+      const scrollLeft = scrollPercentage * (scrollWidth - window.innerWidth);
+
+      gsap.to(containerRef.current, {
+        x: -scrollLeft,
+        duration: 0.2,
+        delay: 3,
+        ease: "power3.out",
+        overwrite: "auto",
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const onScroll = (event) => {
+      gsap.to(containerRef1.current, {
+        // gsap.to("#zoomable-logo", {
+        scale: 200,
+        scrollTrigger: {
+          trigger: "#zoomable-logo",
+          start: "top 75%",
+          end: "bottom left +=500 +=500",
+          //scrub: true,
+          // snap: {
+          //   snapTo: "labels",
+          //   duration: { min: 0.2, max: 10 },
+          //   delay: 3,
+          // },
+        },
+      });
+    };
+    window.addEventListener("scroll", onScroll);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+    // window.addEventListener("wheel", onScroll);
+    // return () => {
+    //   window.removeEventListener("wheel", onScroll);
+    // };
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="overflow-x-hidden">
+      <SmoothScroll />
+      <div className="relative h-screen bg-black overflow-x-hidden overflow-y-hidden">
+        <svg
+          ref={containerRef1}
+          id="zoomable-logo"
+          className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-12 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          width="184.896"
+          height="50"
+          viewBox="0 0 184.896 50"
+        >
+          <path
+            id="Path_7808"
+            data-name="Path 7808"
+            d="M231.6,160.265v-1.046a16.737,16.737,0,0,0-1.046-4.949c-1.155-2.888-3.1-5.6-4.758-8.47-3.069-5.325-6.523-11.478-9.612-16.844-2.543-4.418-4.914-10.1-11.229-10.183-6.567-.091-8.751,5.708-11.134,9.9-7.567,13.3-23.031,40.066-23.031,40.066h7.137s14.755-26.108,22.079-39.3c.936-1.688,2.793-3.9,4.949-3.9,3.27,0,4.906,3.893,6.376,6.567,3.935,7.163,7.727,14.018,11.42,20.936,1.161,2.177,3.517,5.04,1.9,7.994-1.053,1.927-3.174,1.81-6.186,1.81,0,.049-29.121,0-29.121,0v5.9H215.8a62.376,62.376,0,0,0,8.754-.285C228.735,167.665,231.006,164.424,231.6,160.265Z"
+            transform="translate(-46.703 -118.772)"
+            fill="#fff"
+          />
+          <g id="Group_3285" data-name="Group 3285">
+            <g id="Group_3282" data-name="Group 3282">
+              <path
+                id="Path_7809"
+                data-name="Path 7809"
+                d="M93.858,118.772v1.237a2.5,2.5,0,0,1-.494,1.448L61.632,165.768H93.576v3H56.826v-1.308a2.174,2.174,0,0,1,.425-1.272l31.8-44.418H57.745v-3Z"
+                transform="translate(-56.826 -118.772)"
+                fill="#fff"
+              />
+            </g>
+            <g
+              id="Group_3283"
+              data-name="Group 3283"
+              transform="translate(49.257)"
+            >
+              <path
+                id="Path_7810"
+                data-name="Path 7810"
+                d="M105.7,168.772h-3.64v-50h3.64Z"
+                transform="translate(-102.065 -118.772)"
+                fill="#fff"
+              />
+            </g>
+            <g
+              id="Group_3284"
+              data-name="Group 3284"
+              transform="translate(68.951)"
+            >
+              <path
+                id="Path_7811"
+                data-name="Path 7811"
+                d="M145.1,156.333c.142-.376.289-.748.442-1.113a9.271,9.271,0,0,1,.513-1.043L165.7,119.443a1.283,1.283,0,0,1,.566-.565,2.282,2.282,0,0,1,.777-.106h2.509v50h-3.18V126.156a18.937,18.937,0,0,1,.107-1.944L146.8,159.161a1.542,1.542,0,0,1-1.413.918h-.566a1.59,1.59,0,0,1-1.412-.918l-20.213-34.983a19.193,19.193,0,0,1,.106,1.978v42.616h-3.145v-50h2.474a2.443,2.443,0,0,1,.795.106,1.255,1.255,0,0,1,.584.565l20.176,34.771A14.409,14.409,0,0,1,145.1,156.333Z"
+                transform="translate(-120.152 -118.772)"
+                fill="#fff"
+              />
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div className="App">
+        <div
+          ref={containerRef}
+          className="flex h-[100vh] w-[400vw] scroll-snap-container"
+        >
+          <section className="flex-shrink-0 w-screen h-screen bg-black text-[#BE9F56] flex items-center justify-center scroll-snap-start">
+            <Image src={Logo} />
+            <div class="text-center mx-3 lg:text-[25px] 2xl:text-[30px] tracking-wider uppercase">
+              <p>We are the global platform.</p>
+              <p class="mt-3">
+                Services in over one hundred and twenty countries.
+              </p>
+            </div>
+          </section>
+          <section className="flex-shrink-0 w-screen h-screen bg-[#737373] text-white flex flex-col items-center justify-center scroll-snap-start">
+            <h2 class="text-[25px] font-normal uppercase tracking-wider">
+              Bringing the world closer together.
+            </h2>{" "}
+            <Image src={Logo} />
+          </section>
+          <section className="flex-shrink-0 w-screen h-screen bg-[#BE9F56] text-white flex items-center justify-center scroll-snap-start">
+            <div class="flex justify-center items-center min-h-screen relative z-10">
+              <h4 class="text-[16px] lg:text-[25px] text-center uppercase text-white tracking-wider ">
+                A REAL ESTATE AND PROPERTY PLATFORM THAT WILL CHANGE THE WORLD.
+              </h4>
+            </div>
+          </section>
+          <section className="flex-shrink-0 w-screen h-screen bg-black text-white flex items-center justify-center scroll-snap-start">
+            <SvgTransition />
+          </section>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Home;
